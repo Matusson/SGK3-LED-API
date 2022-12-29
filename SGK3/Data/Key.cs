c3b29a701578258c309d9d71e4d372525b7bedec
Matusson <41548922+Matusson@users.noreply.github.com>
2022-12-29 18:28:47 +0100

    Move key codes to separate internal class to avoid clutter

---
+++ SGK3/Data/Key.cs
@@ -0,0 +1,127 @@
+﻿namespace SGK3
+{
+    /// <summary>
+    /// Used to identify physical keys.
+    /// </summary>
+    public enum Key
+    {
+        Esc,
+        F1,
+        F2,
+        F3,
+        F4,
+        F5,
+        F6,
+        F7,
+        F8,
+        F9,
+        F10,
+        F11,
+        F12,
+
+        Prtscr,
+        Scroll,
+        Pause,
+
+        GraveTilde,
+        Alpha1,
+        Alpha2,
+        Alpha3,
+        Alpha4,
+        Alpha5,
+        Alpha6,
+        Alpha7,
+        Alpha8,
+        Alpha9,
+        Alpha0,
+        Dash,
+        Equals,
+        Backspace,
+
+        Insert,
+        Home,
+        PgUp,
+
+        NumLock,
+        NumpadSlash,
+        StarMultiply,
+        Minus,
+
+        Tab,
+        Q,
+        W,
+        E,
+        R,
+        T,
+        Y,
+        U,
+        I,
+        O,
+        P,
+        LBracket,
+        Rbracket,
+        LeftSlash,
+
+        Delete,
+        End,
+        PgDn,
+
+        Num7,
+        Num8,
+        Num9,
+        Plus,
+
+        Caps,
+        A,
+        S,
+        D,
+        F,
+        G,
+        H,
+        J,
+        K,
+        L,
+        Semicolon,
+        Quote,
+        Enter,
+
+        Num4,
+        Num5,
+        Num6,
+
+        LShift,
+        Z,
+        X,
+        C,
+        V,
+        B,
+        N,
+        M,
+        Comma,
+        Period,
+        RightSlash,
+        RShift,
+        UpArrow,
+
+        Num1,
+        Num2,
+        Num3,
+        NumEnter,
+
+        LCtrl,
+        WinKey,
+        LAlt,
+        Space,
+        RAlt,
+        SharkoonFnKey,
+        Menu,
+        RCtrl,
+
+        LeftArrow,
+        DownArrow,
+        RightArrow,
+
+        Num0,
+        NumDel
+    }
+}
